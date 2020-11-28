8e5064fb859b2504e7289ee2b1b132874f5e1e92
NhatMinh12 <74853310+NhatMinh12@users.noreply.github.com>
2020-11-29 02:59:42 +0700

    Add files via upload

---
+++ signin page/signin.js
@@ -0,0 +1,11 @@
+let signUpButton = document.getElementById('signUp')
+let signInButton = document.getElementById('signIn')
+let container = document.getElementById('container')
+
+signUpButton.addEventListener('click', function () {
+    container.classList.add('right-panel-active')
+})
+
+signInButton.addEventListener('click', function () {
+    container.classList.remove('right-panel-active')
+})
